bf1a4fb4e02e94f793447b3ee70e904a0ec0a8f9
cresta522 <cresta522@gmail.com>
2019-04-24 02:51:47 +0000

    nodemongit add .git add .

--- src/noImpl.ts
+++ src/noImpl.ts
@@ -1,5 +1,5 @@
 import * as Express from 'express';
 
 export default function notImplemented(req: Express.Request, res: Express.Response, next: Express.NextFunction) {
-  res.status(501).json({ message: 'Not Implemented.' });
+  res.status(501).json({ message: 'Not Implemented....' });
 }
